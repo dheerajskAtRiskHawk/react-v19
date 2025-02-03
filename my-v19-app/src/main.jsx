b8dd3b003b02837d1ca184d6b11974cb1a26becb
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2025-02-03 23:19:07 +0530

    new project

--- my-v19-app/src/main.jsx
+++ my-v19-app/src/main.jsx
@@ -2,11 +2,9 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
-import PostForm from './PostForm.jsx'
-import PostForm2 from './POForm.jsx'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <PostForm2 />
+    <App />
   </StrictMode>,
 )
