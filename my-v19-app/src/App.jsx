cb86d4e035103f21062899b4070eae766a99f79a
Dheeraj Kumar <dheeraj.s.roy@gmail.com>
2025-02-05 19:12:00 +0530

    ref as prop

--- my-v19-app/src/App.jsx
+++ my-v19-app/src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { useRef, forwardRef } from "react";
 
-function App() {
-  const [count, setCount] = useState(0)
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is = {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+const InputField = (props, ref) => {
+  return <input className="form-control mb-2" ref={ref} {...props}/>;
+};
+
+
+export default function App() {
+
+  const inputRef = useRef(null); //placeholder to store the reference of element.
+
+  const focusInput = ()=>{
+    console.log('focusInput');
+    if(inputRef.current){
+      inputRef.current.focus();
+    }
+    // get the reference of input from child component. and call focus method on it.
+  }
+ 
+return (
+    <div className="container">
+      <InputField ref={inputRef} placeholder="Type here..." />
+      <button onClick={focusInput} >Focus Input</button>
+    </div>
+  );
 }
-
-export default App
